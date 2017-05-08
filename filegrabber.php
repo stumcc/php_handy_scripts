c941f6a82a03824a0f22d89bb250e30e717b68d2
root <root@ip-10-0-0-10.eu-west-1.compute.internal>
2017-05-08 10:52:54 +0000

    have added in the php script

---
+++ filegrabber.php
@@ -0,0 +1,9 @@
+<?php
+# Define the file you want to download
+$target = "http://54.229.13.50/test.html";
+$file_handle = fopen($target, "r");
+
+# Fetch the file
+while (!feof($file_handle))
+    echo fgets($file_handle, 4096);
+fclose($file_handle); ?>
